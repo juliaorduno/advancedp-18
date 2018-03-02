1e44f65b00da2891397c6764e64cd372374b4df7
Julia Orduño <juliapaola97@gmail.com>
2018-03-01 20:28:20 -0600

    práctica 7

---
+++ practicas/practica6/hash.c
@@ -0,0 +1,106 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include "hash.h"
+#define HASH_DEBUG "ENV_DEBUG"
+
+void printDebug(char *msg)
+{
+    char hashDebug = getenv(HASH_DEBUG);
+    if (hashDebug != NULL && strcmp(hashDebug, "y") == 0)
+    {
+        printf("%s\n", msg);
+    }
+}
+
+unsigned long dohash(unsigned char *str, unsigned size)
+{
+    unsigned long hash = 5381;
+    int c;
+
+    while (c = *str++)
+        hash = ((hash << 5) + hash) + c;
+
+    return hash % size;
+}
+
+void hashInit(HashInt *hash, int size)
+{
+    hash->size = size;
+    hash->data = (HashElement *)calloc(size, size * sizeof(HashElement));
+}
+
+static void hashDoInsert(HashInt *hash, char *key, int value)
+{
+    char errorMessage[255];
+    printDebug("dohash");
+    unsigned index = dohash(key, hash->size);
+    printDebug("dohash2");
+    HashElement *currentHashElement = &(hash->data[index]);
+    sprintf(errorMessage, "index %d\n", index);
+    printDebug(errorMessage);
+    if (currentHashElement->elements == NULL)
+    {
+        currentHashElement->elements = (Element *)malloc(2 * sizeof(Element));
+        currentHashElement->count = 0;
+        currentHashElement->length = 2;
+    }
+    if (currentHashElement->count == currentHashElement->length)
+    {
+        currentHashElement->length *= 2;
+        currentHashElement->elements = (Element *)realloc(currentHashElement->elements, currentHashElement->length * sizeof(Element));
+    }
+    currentHashElement->elements[currentHashElement->count].key = strdup(key);
+    currentHashElement->elements[currentHashElement->count].value = value;
+    currentHashElement->count += 1;
+}
+
+static void hashDoUpdate(HashInt *hash, char *key, int value)
+{
+    int pos = dohash(key, hash->size);
+    int i;
+    Element *currentElements = hash->data[pos].elements;
+    int currentElementsLength = hash->data[pos].count;
+
+    for (i = 0; i < currentElementsLength; i++)
+    {
+        if (strcmp(key, currentElements[i].key) == 0)
+        {
+            currentElements[i].value = value;
+            break;
+        }
+    }
+}
+
+void hashInsert(HashInt *hash, char *key, int value)
+{
+    int *oldValue = hashGet(hash, key);
+    if (oldValue == NULL)
+    {
+        hashDoInsert(hash, key, value);
+    }
+    else
+    {
+        hashDoUpdate(hash, key, value);
+    }
+}
+
+int *hashGet(HashInt *hash, char *key)
+{
+    int pos = dohash(key, hash->size);
+    int i;
+    Element *currentElements = hash->data[pos].elements;
+    int currentElementsLength = hash->data[pos].count;
+    int *value = NULL;
+
+    for (i = 0; i < currentElementsLength; i++)
+    {
+        if (strcmp(key, currentElements[i].key) == 0)
+        {
+            value = (int *)malloc(sizeof(int));
+            *value = currentElements[i].value;
+            break;
+        }
+    }
+    return value;
+}
---
+++ practicas/practica6/hash.h
@@ -0,0 +1,22 @@
+typedef struct e
+{
+    char *key;
+    int value;
+} Element;
+
+typedef struct d
+{
+    Element *elements;
+    unsigned count;
+    unsigned length;
+} HashElement;
+
+typedef struct h
+{
+    HashElement *data;
+    int size;
+} HashInt;
+
+void hashInit(HashInt *, int);
+void hashInsert(HashInt *, char *, int);
+int * hashGet(HashInt *, char *);
